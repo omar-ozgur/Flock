9529c2943af69467102c787373215e8379895fb7
Omar Ozgur <oozgur217@gmail.com>
2017-08-12 16:21:08 -0700

    Added comments to login controller

--- app/controllers/login.go
+++ app/controllers/login.go
@@ -8,40 +8,39 @@ import (
 	"net/http"
 )
 
+// Login with Facebook
 func LoginWithFacebook(w http.ResponseWriter, r *http.Request) {
+
+	// Set headers
 	w.Header().Set("Content-Type", "application/json")
 
+	// Get body parameters
 	b, _ := ioutil.ReadAll(r.Body)
 	JSONData, _ := jason.NewObjectFromBytes(b)
 	token, _ := JSONData.GetString("token")
 
+	// Get Facebook response
 	response, _ := http.Get("https://graph.facebook.com/me?access_token=" + token + "&fields=email,first_name,last_name,id,friends")
-
 	defer response.Body.Close()
 
+	// Get user details
 	body, _ := ioutil.ReadAll(response.Body)
-
 	user, _ := jason.NewObjectFromBytes([]byte(body))
-
 	first_name, _ := user.GetString("first_name")
 	last_name, _ := user.GetString("last_name")
 	email, _ := user.GetString("email")
-	fb_id_string, _ := user.GetString("id")
+	fb_id, _ := user.GetString("id")
 
-	status, message, app_token := models.ProcessFBLogin(first_name, last_name, email, fb_id_string)
+	// Internal Facebook login
+	status, message, app_token := models.ProcessFBLogin(first_name, last_name, email, fb_id)
 
+	// Return a response
 	JSON, _ := json.Marshal(map[string]interface{}{
 		"status":    status,
 		"message":   message,
 		"fb_token":  token,
 		"app_token": app_token,
 	})
-
 	w.Write(JSON)
 
-}
-
-type LoggedInPageAttr struct {
-	Name string
-	URL  string
 }
